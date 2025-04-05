e9ad9dc9dc3df27247712375e2f1268aad815270
Thái Văn Phúc <phuc100662@donga.edu.vn>
2025-04-05 20:23:22 +0700

    update

--- back-end/src/middlewares/authMiddleware.js
+++ back-end/src/middlewares/authMiddleware.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401); // Unauthorized
+  if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, "SECRET_KEY", (err, user) => {
-    if (err) return res.sendStatus(403); // Forbidden
+  jwt.verify(token, "secretKey", (err, user) => {
+    if (err) return res.sendStatus(403);
     req.user = user;
     next();
   });
