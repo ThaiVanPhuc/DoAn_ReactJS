ab62324aa9affe6316275b74d3546dba5bec4e90
Thái Văn Phúc <128126363+ThaiVanPhuc@users.noreply.github.com>
2025-11-06 07:26:10 +0700

    update

--- front-end/src/utils/httpRequest.js
+++ front-end/src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpRequest = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "/api/",
 });
 
 httpRequest.interceptors.request.use((config) => {
